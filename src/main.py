59b1d1f213fb19c14b718b2c6fec4630705be36d
hore1014 <hore1014@users.noreply.github.com>
2021-12-02 21:52:41 +0100

    added future arrival of orders to procurement planning

--- src/main.py
+++ src/main.py
@@ -10,14 +10,18 @@ from functools import reduce
 db.init_db()
 
 # load all necessary data for production calculation from database
-#TODO period is user input
-sales_forecast = db.get_sales_forecast(1)
-current_parts = db.get_parts_inventory(1)
-planned_parts = mock.get_inventory_strategy(1)
-parts_processing = db.get_parts_processing(1)
-parts_in_queue = db.get_parts_in_queue(1)
-missing_parts = db.get_missing_parts(1)
-parts_traded = db.get_parts_trade(1)
+# TODO period is user input
+current_period = 2
+
+sales_forecast = mock.get_sales_forecast(current_period)
+current_parts = mock.get_parts_inventory(current_period)
+planned_parts = mock.get_inventory_strategy(current_period)
+parts_processing = mock.get_parts_processing(current_period)
+parts_in_queue = mock.get_parts_in_queue(current_period)
+# missing_parts = db.get_missing_parts(current_period)
+parts_traded = mock.get_parts_trade(current_period)
+parts_ordered = mock.get_orders_in_transit(
+    current_period-1) if current_period > 1 else {}
 
 production = prod.calculate_production(
     sales_forecast,
@@ -42,16 +46,20 @@ consumption2 = cons.calculate_consumption_forecast(production2)
 consumption3 = cons.calculate_consumption_forecast(production3)
 consumption4 = cons.calculate_consumption_forecast(production4)
 
+
 orders = proc.calculate_procurement(current_parts, consumption1,
-                                    consumption2, consumption3, consumption4)
+                                    consumption2, consumption3, consumption4, parts_ordered, current_period)
 
 # TODO: calculate tooling factors for each period from average of pervious periods
 tooling_factors = {"Station_1": 1.25, "Station_2": 1.25, "Station_3": 1, "Station_4": 1.25, "Station_5": 0, "Station_6": 1, "Station_7": 2,
                    "Station_8": 1.5, "Station_9": 1, "Station_10": 1.25, "Station_11": 1.25, "Station_12": 1, "Station_13": 1, "Station_14": 1, "Station_15": 2, }
 
+# TODO: take a look at capacity calculation because results are off for stations 6 - 13
 capacity = cap.calculate_capacity(production, tooling_factors)
 
 shifts = cap.calculate_shifts(capacity)
+
+# TODO: calculate inventory at end of period and check if inventory worth is below 250.000€
 
 # print the results with sums for control purposes
 print(f"Produktion:\n{json.dumps(production, indent=4)}")
@@ -73,5 +81,5 @@ print("}\n" +
 print("Bestellungen:\n{")
 for article in orders:
     print(
-        f"\t\"{article}\": Normalbestellung: {orders[article][0]}, Eilbestellung: {orders[article][1]}")
+        f"\t\"{article}\": Normalbestellung: {orders[article][0]},\tEilbestellung: {orders[article][1]},\tReichweite: {orders[article][2]} Tage")
 print("}\n")
--- src/procurement/procurement.py
+++ src/procurement/procurement.py
@@ -1,17 +1,24 @@
 from procurement import lookupProcurement as pc
-from math import floor
+from math import floor, ceil
 
 
-def calculate_procurement(inventory, forecast0, forecast1, forecast2, forecast3):
-    results = {"K21": (0, 0), "K22": (0, 0), "K23": (0, 0), "K24": (0, 0), "K25": (0, 0), "K27": (0, 0), "K28": (0, 0), "K32": (0, 0), "K33": (0, 0), "K34": (0, 0), "K35": (0, 0), "K36": (0, 0), "K37": (0, 0), "K38": (0, 0), "K39": (
-        0, 0), "K40": (0, 0), "K41": (0, 0), "K42": (0, 0), "K43": (0, 0), "K44": (0, 0), "K45": (0, 0), "K46": (0, 0), "K47": (0, 0), "K48": (0, 0), "K52": (0, 0), "K53": (0, 0), "K57": (0, 0), "K58": (0, 0), "K59": (0, 0), }
+def calculate_procurement(inventory, forecast0, forecast1, forecast2, forecast3, orders, period):
+    results = {"K21": (0, 0, 0), "K22": (0, 0, 0), "K23": (0, 0, 0), "K24": (0, 0, 0), "K25": (0, 0, 0), "K27": (0, 0, 0), "K28": (0, 0, 0), "K32": (0, 0, 0), "K33": (0, 0, 0), "K34": (0, 0, 0), "K35": (0, 0, 0), "K36": (0, 0, 0), "K37": (0, 0, 0), "K38": (0, 0, 0), "K39": (
+        0, 0, 0), "K40": (0, 0, 0), "K41": (0, 0, 0), "K42": (0, 0, 0), "K43": (0, 0, 0), "K44": (0, 0, 0), "K45": (0, 0, 0), "K46": (0, 0, 0), "K47": (0, 0, 0), "K48": (0, 0, 0), "K52": (0, 0, 0), "K53": (0, 0, 0), "K57": (0, 0, 0), "K58": (0, 0, 0), "K59": (0, 0, 0), }
+
+    # TODO: test this factor in sandbox environment
+    order_factor = 1.2
 
     for article in results:
         inventory_reach = 0
+        orders_article = orders[article] if article in orders else []
+
         average_consumption = (
             forecast0[article] + forecast1[article] + forecast2[article] + forecast3[article])/4
-        order = average_consumption * 1.2
+        # factor in a buffer when ordering and round up to next 10
+        order = int(ceil(average_consumption * order_factor / 10) * 10)
 
+        # check in which period inventory goes to 0 and then find exact day to calulate inventory reach
         if (inventory[article] - forecast0[article] <= 0):
             inventory_reach = floor(
                 0 + (inventory[article]) / (forecast0[article]/5))
@@ -28,15 +35,33 @@ def calculate_procurement(inventory, forecast0, forecast1, forecast2, forecast3)
             inventory_reach = floor(
                 15 + (inventory[article] - forecast0[article] - forecast1[article] - forecast2[article]) / (forecast3[article]/5))
 
-        elif ():
-            print(f"Reichweite Lagerbestand {article}: >20 Tage")
-
-        if (inventory_reach < pc.delivery_days[article][2]):
-
-            results[article] = (0, order)
-        elif (inventory_reach >= pc.delivery_days[article][2] and inventory_reach <= pc.delivery_days[article][2] + 5):
-            results[article] = (order, 0)
         else:
-            results[article] = (0, 0)
+            inventory_reach = 20
+
+        add_to_reach = 0
+        for order_unit in orders_article:
+            # check if order is normal (5) or express (4) and get the corresponding delivery time
+            order_arrival = pc.delivery_days[article][0] if order_unit[2] == 4 else pc.delivery_days[article][2]
+            # calculate how many periods ago the order was issued and subtract 5 days per period
+            order_arrival -= (period - order_unit[0])*5
+
+            # check if order arrives before inventory goes to 0 and if yes, add the amount of days gained
+            if (inventory_reach >= order_arrival):
+                add_to_reach += (order_unit[1] / (average_consumption/5))
+
+        inventory_reach = floor(inventory_reach + add_to_reach)
+
+        # express order if reach is less than normal delayed delivery time
+        if (inventory_reach < pc.delivery_days[article][2]):
+            # for express orders order double the amount so next period a normal order can be issued
+            results[article] = (0, order*2, inventory_reach)
+
+        # normal order if reach is greater than normal delayed delivery time and if inventory has less than 5 days of buffer
+        elif (inventory_reach >= pc.delivery_days[article][2] and inventory_reach <= pc.delivery_days[article][2] + 5):
+            results[article] = (order, 0, inventory_reach)
+
+        # no order if inventory has at least 5 days of buffer
+        else:
+            results[article] = (0, 0, inventory_reach)
 
     return results
