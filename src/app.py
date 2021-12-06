aa22b430423fe55b032c95a77b180a8b7fbbcde0
hore1014 <hore1014@users.noreply.github.com>
2021-12-06 22:01:09 +0100

    more connections between frontend and backend

--- src/app.py
+++ src/app.py
@@ -5,40 +5,46 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 port = 5000  # default
 
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.xml']
-app.config['UPLOAD_PATH'] = 'src/data/'
+app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024  # max 1MB upload size
+app.config["UPLOAD_EXTENSIONS"] = [".xml"]
+app.config["UPLOAD_PATH"] = "src/data/"
+app.config["SECRET_KEY"] = os.urandom(24).hex()  # random key
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/1_lastPeriod.html')
+@app.route("/1_lastPeriod.html")
 def lastPeriod():
-    return render_template('1_lastPeriod.html')
+    return render_template("1_lastPeriod.html", error=False)
 
 
-@app.route('/1_lastPeriod.html', methods=['POST'])
+@app.route("/1_lastPeriod.html", methods=["POST"])
 def upload_file():
-    uploaded_file = request.files['file']
+    if "file" not in request.files:
+        abort(400)
+    uploaded_file = request.files["file"]
     filename = secure_filename(uploaded_file.filename)
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-            abort(400)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-        print(
-            f"File '{filename}' uploaded to {os.path.join(app.config['UPLOAD_PATH'], filename)}")
-        return redirect(url_for('salesPrediction'))
-    else:
-        return redirect(url_for('lastPeriod'))
+    if filename == "":
+        return render_template("1_lastPeriod.html", error=True)
+    file_ext = os.path.splitext(filename)[1]
+    if file_ext not in app.config["UPLOAD_EXTENSIONS"]:
+        abort(415)
+    uploaded_file.save(os.path.join(app.config["UPLOAD_PATH"], filename))
+    # TODO: hier muss ggf. bereits veranlasst werden, dass die xml datei eingelesen wird
+    return redirect(url_for("salesPrediction"))
 
 
-@app.route('/2_salesPrediction.html')
+@app.route("/2_salesPrediction.html")
 def salesPrediction():
-    return render_template('2_salesPrediction.html')
+    return render_template("2_salesPrediction.html", data_1_1=100, data_1_2=100, data_1_3=100, data_1_4=100, data_2_1=100, data_2_2=100, data_2_3=100, data_2_4=100, data_3_1=100, data_3_2=100, data_3_3=100, data_3_4=100)
+
+
+@app.route("/2_salesPrediction.html", methods=["POST"])
+def upload_prediction():
+    pass
 
 
 if __name__ == "__main__":
