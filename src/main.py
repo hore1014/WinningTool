19ea3c119f2aeb047c63c9c45977e7dab5a5ad38
hore1014 <hore1014@users.noreply.github.com>
2021-12-09 12:17:30 +0100

    ordered functions in main

--- src/main.py
+++ src/main.py
@@ -123,25 +123,58 @@ def get_production(current_period):
         missing_parts,
         parts_traded,
     )
+
+    print(f"Produktion:\n{json.dumps(production, indent=4)}")
     return production
 
 
-def get_all_from_db(current_period):
+def get_consumption():
+    global consumption
+    consumption = cons.calculate_consumption(production, sales_forecast)
 
-    global parts_ordered
+    print(f"Verbrauch:\n{json.dumps(consumption, indent=4)}")
+    return consumption
 
-    parts_ordered = db.get_orders_in_transit(
-        current_period-1) if current_period > 1 else {}
+
+def get_capacity():
+    # TODO: calculate tooling factors for each period from average of pervious periods
+    tooling_factors = {"Station_1": 1.25, "Station_2": 1.25, "Station_3": 1, "Station_4": 1.25, "Station_5": 0, "Station_6": 1, "Station_7": 2,
+                       "Station_8": 1.5, "Station_9": 1, "Station_10": 1.25, "Station_11": 1.25, "Station_12": 1, "Station_13": 1, "Station_14": 1, "Station_15": 2, }
+
+    global capacity
+    capacity = cap.calculate_capacity(
+        production,
+        parts_processing,
+        parts_in_queue,
+        missing_parts,
+        tooling_factors
+    )
+
+    print(f"Kapazität:\n{json.dumps(capacity, indent=4)}")
+    return capacity
+
+
+def get_shifts():
+    global shifts
+    shifts = cap.calculate_shifts(capacity)
+
+    print("Schichten:\n{")
+    for station in shifts:
+        print(
+            f"\t\"{station}\": ({shifts[station][0]}, {shifts[station][1]/5})")
+
+    return shifts
 
 
 def calc_prod_forecast(period):
     return prod.calculate_production_forecast(sales_forecast, planned_parts, period)
 
 
-def calculate_all_data(current_period):
+def get_orders():
+    global parts_ordered
 
-    global consumption
-    consumption = cons.calculate_consumption(production, sales_forecast)
+    parts_ordered = db.get_orders_in_transit(
+        current_period-1) if current_period > 1 else {}
 
     production2 = calc_prod_forecast(1)
     production3 = calc_prod_forecast(2)
@@ -153,27 +186,42 @@ def calculate_all_data(current_period):
     consumption4 = cons.calculate_consumption_forecast(production4)
 
     global orders
-    orders = proc.calculate_procurement(current_parts, consumption1,
-                                        consumption2, consumption3, consumption4, parts_ordered, current_period)
+    orders = proc.calculate_procurement(
+        current_parts,
+        consumption1,
+        consumption2,
+        consumption3,
+        consumption4,
+        parts_ordered,
+        current_period)
 
-    # TODO: calculate tooling factors for each period from average of pervious periods
-    tooling_factors = {"Station_1": 1.25, "Station_2": 1.25, "Station_3": 1, "Station_4": 1.25, "Station_5": 0, "Station_6": 1, "Station_7": 2,
-                       "Station_8": 1.5, "Station_9": 1, "Station_10": 1.25, "Station_11": 1.25, "Station_12": 1, "Station_13": 1, "Station_14": 1, "Station_15": 2, }
+    print("Bestellungen:\n{")
+    for article in orders:
+        print(
+            f"\t\"{article}\": Normalbestellung: {orders[article][0]},\tEilbestellung: {orders[article][1]},\tReichweite: {orders[article][2]} Tage")
+    print("}\n")
 
-    global capacity
-    capacity = cap.calculate_capacity(
-        production, parts_processing, parts_in_queue, missing_parts, tooling_factors)
+    return orders
 
-    global shifts
-    shifts = cap.calculate_shifts(capacity)
 
+def get_average_inventory_worth():
     global average_inventory_worth
+
+    get_consumption()
+
     average_inventory_worth = inv.get_average_inventory_worth(
         current_parts, production, consumption, parts_processing, parts_in_queue, missing_parts, parts_traded)
 
+    print(
+        f"Durchschnittlicher Lagerwert:\n{json.dumps(average_inventory_worth, indent=4)}")
+    return average_inventory_worth
+
+
+# Testing
+get_production(1)
+
+
 # print the results with sums for control purposes
-
-
 def print_all_data():
     print(f"Produktion:\n{json.dumps(production, indent=4)}")
     print(
