60142db3cde8e57dbaae13482e5a74339c3bccba
Boehm <david.boehm@sap.com>
2021-12-12 02:02:16 +0100

    heroku configs

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
-from app.main import app
+from src.main import app, port
  
 if __name__ == "__main__":
     # "debug=True" refreshes app every time a change is made, but Debugging is only possible for "debug=False"
-    app.run(debug=True)
+    app.run(debug=True, port = port)
