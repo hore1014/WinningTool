2e99fd87b4151e1f566a3fb557091460532fcec8
Boehm <david.boehm@sap.com>
2021-12-14 01:16:54 +0100

    fixed bug for orders in export xml

--- src/main.py
+++ src/main.py
@@ -452,6 +452,8 @@ def upload_orders():
     for article in lookupArticles.k_list:
         norm = request.form.get(f"normal_{article}")
         eil = request.form.get(f"express_{article}")
+        if norm == 0 and eil == 0:
+            continue
         if norm != 0:
             orders.append((  # tuple
                 article,
